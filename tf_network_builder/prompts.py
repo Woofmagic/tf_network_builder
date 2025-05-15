52642b75b1854626f3e30f399d734a6f22e553a2
MidnightExcursion <dima.watkins@gmail.com>
2025-05-15 11:28:35 -0500

    added two relevant prompts in file

---
+++ tf_network_builder/prompts.py
@@ -0,0 +1,4 @@
+# prompts.py
+
+PROMPT_NUMBER_OF_LAYERS = "> How many layers do you want in your ANN architecture?"
+ERROR_INVALID_LAYER_INPUT = "> Layer number must be a positive, nonzero integer. Try again."
