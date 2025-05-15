bb283090e22f29d49effe0508a31ed999c8889e0
MidnightExcursion <dima.watkins@gmail.com>
2025-05-15 13:18:10 -0500

    added preliminary validation function

--- tf_network_builder/validation.py
+++ tf_network_builder/validation.py
@@ -1,6 +1,78 @@
 # validation.py
 
-def get_positive_integer(prompt_template, error_message, preprovided_value = None, verbose = False):
+
+from .prompts import ERROR_INVALID_LAYER_INPUT, ERROR_INVALID_NODES_INPUT
+from .prompts import ERROR_ACTIVATION_MISMATCH, ERROR_UNKNOWN_LOSS
+from .losses import LOSS_FUNCTION_MAP
+from .activations import ALLOWED_ACTIVATIONS
+
+def validate_config(configuration_dictionary: dict, verbose: bool):
+    """
+    ## Description:
+    Validate user's dict of initialization parameters.
+    
+    ## Parameters:
+    configuration_dictionary (dict):
+        a dict of user-requests DNN parameters that 
+        will be used to attempt to build a TF network.
+
+    verbose (boolean):
+        Do you want to see all output of this function evaluation?
+
+    ## Notes
+    
+    """
+    
+    try:
+        number_of_hidden_layers = int(configuration_dictionary["num_layers"])
+        if number_of_hidden_layers <= 0:
+            raise ValueError(ERROR_INVALID_LAYER_INPUT)
+        
+    except Exception:
+        raise ValueError(ERROR_INVALID_LAYER_INPUT)
+
+    # (): We now retrieve the *list* of integers characterising nodes per hidden layer:
+    nodes_per_layer = configuration_dictionary.get("nodes_per_layer")
+
+    # (): We now actually valid the *list* of *integers* that is provided in the dictionary:
+    if not (isinstance(nodes_per_layer, list) and len(nodes_per_layer) == number_of_hidden_layers and all(isinstance(node_number, int) for node_number in nodes_per_layer)):
+        raise ValueError(ERROR_INVALID_NODES_INPUT)
+    
+    else:
+        if verbose:
+            print("> Validated list of nodes per layer!")
+
+    activations = configuration_dictionary.get("activations")
+
+    if not (isinstance(activations, list) and len(activations) == number_of_hidden_layers and all(act in ALLOWED_ACTIVATIONS for act in activations)):
+        raise ValueError(ERROR_ACTIVATION_MISMATCH)
+    
+    else:
+        if verbose:
+            print("> Validated list of activations!")
+
+    loss_name = configuration_dictionary.get("loss")
+
+    if loss_name not in LOSS_FUNCTION_MAP:
+        raise ValueError(ERROR_UNKNOWN_LOSS)
+    else:
+        if verbose:
+            print("> Validated global loss function choice!")
+
+    network_input_dimension = configuration_dictionary.get("input_dimension")
+
+    if not isinstance(network_input_dimension, int) or network_input_dimension <= 0:
+        raise ValueError("> Input dimension must be a positive integer.")
+
+    return {
+        "num_layers": number_of_hidden_layers,
+        "nodes_per_layer": nodes_per_layer,
+        "activations": activations,
+        "loss": LOSS_FUNCTION_MAP[loss_name],
+        "input_dim": network_input_dimension,
+    }
+
+def get_positive_integer(prompt_template, error_message, preprovided_value = None, verbose: bool = False):
     """
     ## Description:
     Obtain the number of hidden layers for the ANN from the user.
