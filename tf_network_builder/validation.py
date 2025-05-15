8510cd94520f3046e23c7dcb65b49da2bb7b479b
MidnightExcursion <dima.watkins@gmail.com>
2025-05-15 11:49:10 -0500

    added validation for the second layer of the network builder

--- tf_network_builder/validation.py
+++ tf_network_builder/validation.py
@@ -1,11 +1,23 @@
 # validation.py
 
-def get_positive_integer(prompt, error_message, preprovided_value = None, verbose = False):
+def get_positive_integer(prompt_template, error_message, preprovided_value = None, verbose = False):
     """
     ## Description:
     Obtain the number of hidden layers for the ANN from the user.
     
     ## Parameters:
+    prompt_template (string):
+        queue the CLI prompt
+
+    error_message (string):
+        queue the corresponding error message
+
+    preprovided_value (string):
+        if the use wants to specify a dictionary of 
+        customized hyperparameters specifying a given DNN
+        architecture, then we allow this string argument
+        to bypass CLI interactivity
+    
     verbose (boolean):
             Do you want to see all output of this function evaluation?
             
@@ -31,7 +43,7 @@ def get_positive_integer(prompt, error_message, preprovided_value = None, verbos
 
         try:
 
-            number_of_ann_layers = int(input(prompt))
+            number_of_ann_layers = int(input(prompt_template))
             if number_of_ann_layers <= 0:
                 print(error_message)
                 continue
@@ -42,3 +54,63 @@ def get_positive_integer(prompt, error_message, preprovided_value = None, verbos
         
         except ValueError:
             print(error_message)
+
+def get_layerwise_integers(prompt_template, error_message, number_of_hidden_layers, preprovided_values = None, verbose = False):
+    """
+    ## Description:
+    Obtain the number of nodes per layer in the ANN.
+
+    ## Parameters:
+    prompt_template (string):
+        queue the CLI prompt
+
+    error_message (string):
+        queue the corresponding error message
+
+    number_of_hidden_layers (int):
+        the number of hidden layers in the DNN
+        as specified previously
+
+    preprovided_value (string):
+        if the use wants to specify a dictionary of 
+        customized hyperparameters specifying a given DNN
+        architecture, then we allow this string argument
+        to bypass CLI interactivity
+    
+    verbose (boolean):
+            Do you want to see all output of this function evaluation?
+    
+    ## Notes:
+
+    (1) For all each layer, we need to populate it with a number of neurons.
+        So, this function is about obtaining a list of intergers that correspond
+        to the number of neurons per layer.
+
+    (2) The output is a list of integers.
+    """
+    if preprovided_values is not None:
+
+        if isinstance(preprovided_values, list):
+            if len(preprovided_values) != 0:
+                if verbose:
+                    print(f"> Using pre-provided values of: {preprovided_values}")
+                return preprovided_values
+        
+        else:
+            raise ValueError(error_message)
+        
+    list_of_number_of_nodes_per_layer = []
+
+    for hidden_layer_index in range(number_of_hidden_layers):
+        while True:
+            try:
+                value = int(input(prompt_template.format(hidden_layer_index + 1)))
+                if verbose:
+                    print(f"> Received input: {value} ({type(value)}).")
+                list_of_number_of_nodes_per_layer.append(value)
+                break
+
+            except ValueError:
+                print(error_message)
+
+    return list_of_number_of_nodes_per_layer
